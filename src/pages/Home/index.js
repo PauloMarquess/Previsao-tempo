26635c782f7cbfaf7af77084a1bbe2a9cf045b85
PauloMarquess <paulinho_marques2012@hotmail.com>
2021-12-28 13:51:50 -0300

    finalizado o projeto com responsividade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,65 +1,65 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { Btn, Container,Main } from './styled'
-
+import { Btn, Container, Main } from "./styled";
 
 const Home = () => {
+  const [city, setCity] = useState("");
 
-    const [city, setCity] = useState('')
+  const [weatherForcast, setWeatherForcast] = useState(null);
 
-    const [weatherForcast,setWeatherForcast] =useState(null)
-
-    const searcForcastWeather = () => {
-        
-        fetch(
-            `http://api.weatherapi.com/v1/current.json?key=b60f38e95f5b496f97c163757212210&q=${city}&lang=pt`
-            ).then((response) => {
-                if (response.status===200){
-                    return response.json()
-                }
-                if (response.starus !==200){
-                    alert('Erro! Local não encontrado')
-                }
-            }).then ((data)=>{
-                setWeatherForcast(data)
-            })
-            setCity('')
-    }
-    
-
-    const handleCityChange = (e) => {
-        setCity(e.target.value)
-    }
-
-    return (
-        <Main>
-            
-            <Container>
-                <h1>Verifique agora a previsão do tempo na sua cidade !</h1>
-                <p>Digite a sua cidade no campo abaixo e em seguida clique em pesquisar </p>
-                <input value={city} onChange={handleCityChange} />
-                <Btn onClick={searcForcastWeather }  >Pesquisar </Btn>
-
-                {
-                    weatherForcast ? (
-                        <div>
-                            <div>
-                                <img src={weatherForcast.current.condition.icon} alt="Tempo"/>
-                            </div>
-                            <div>
-                                <h3>Hoje o dia está : {weatherForcast.current.condition.text} 
-                                na cidade de  {weatherForcast.location.name} - {weatherForcast.location.region}</h3>
-                            </div>
-                            <div>
-                               <h3> Temp :{weatherForcast.current.temp_c}°C</h3>
-                            </div>
-                        </div>
-
-                    ) : null
-                }
-            </Container>
-        </Main>
+  const searcForcastWeather = () => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=b60f38e95f5b496f97c163757212210&q=${city}&lang=pt`
     )
-}
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        if (response.starus !== 200) {
+          alert("Erro! Local não encontrado");
+        }
+      })
+      .then((data) => {
+        setWeatherForcast(data);
+        console.log(data);
+      });
+    setCity("");
+  };
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  return (
+    <Main>
+      <Container>
+        <h1>Verifique agora a previsão do tempo na sua cidade !</h1>
+        <p>
+          Digite a sua cidade no campo abaixo e em seguida clique em pesquisar{" "}
+        </p>
+        <input value={city} onChange={handleCityChange} />
+        <Btn onClick={searcForcastWeather}>Pesquisar </Btn>
+
+        {weatherForcast ? (
+          <div>
+            <div>
+              <img src={weatherForcast.current.condition.icon} alt="Tempo" />
+            </div>
+            <div>
+              <h3>
+                Hoje o dia está : {weatherForcast.current.condition.text}
+                na cidade de {weatherForcast.location.name} -{" "}
+                {weatherForcast.location.region}
+              </h3>
+            </div>
+            <div>
+              <h3> Temp :{weatherForcast.current.temp_c}°C</h3>
+            </div>
+          </div>
+        ) : null}
+      </Container>
+    </Main>
+  );
+};
 
 export default Home;
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,72 +1,77 @@
-import styled from "styled-components"
-import Clima from '../../assets/img/clima.png'
+import styled from "styled-components";
+import Clima from "../../assets/img/clima.png";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  background-color: #2229;
+  margin: 0 20% 0 24%;
+  padding: 30px;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  h1,
+  p,
+  input {
+    margin: 10px;
+  }
 
-export const Container =styled.div`
-flex-direction: column;
-align-items: center;
-width: 50%;
-background-color: #2229 ;
-margin: 0 20% 0 24%;
-padding: 30px;
-color: #fff;
-border: none;
-border-radius: 3px;
-h1,p,input {
-    margin:10px
-}
-
-p{
+  p {
     margin: 15px;
     font-weight: 600;
-}
-h3 {
-    margin-top:10px
-}
-@media (max-width:768px) {
-   text-align: center;
-   padding: 10px;
-}
+  }
+  h3 {
+    margin-top: 10px;
+  }
+  @media (max-width: 768px) {
+    text-align: center;
+    padding: 10px;
+    width: 80%;
+    margin: 0;
+    margin-inline: auto;
 
-`
+    h3 {
+      font-size: 20px;
+    }
+  }
+`;
 
-export const Btn =styled.button`
-margin: 10px;
-background-color: #013d44;
-color:#fff;
-width: 100px;
-height:40px;
-border-radius:5px;
-border: none;
+export const Btn = styled.button`
+  margin: 10px;
+  background-color: #013d44;
+  color: #fff;
+  width: 100px;
+  height: 40px;
+  border-radius: 5px;
+  border: none;
 
-:hover{
+  :hover {
     cursor: pointer;
     background-color: #5969f7b8;
-}
-`
+  }
+`;
 
-export const Main =styled.main`
+export const Main = styled.main`
+  background-image: url(${Clima});
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 85vh;
+  padding-top: 15%;
+  align-items: center;
+  background-size: cover;
 
-background-image: url(${Clima}) ;
-background-repeat: no-repeat;
-width: 100%;
-height: 85vh;
-padding-top:15% ;
-align-items: center;
-background-size: cover;
-
-
-input {
+  input {
     padding: 5px;
 
-    :focus{
-        box-shadow: 0;
-        outline: 0;
+    :focus {
+      box-shadow: 0;
+      outline: 0;
     }
-}
+  }
 
-@media (max-width:768px) {
+  @media (max-width: 768px) {
     height: 100vh;
-}
-
-`
+  }
+`;
